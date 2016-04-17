60d6d9640dc6b0e465626e1fb29665fe76331e76
Kaspars Dancis <kaspars@fullcontact.com>
2016-04-16 21:09:19 -0600

    Version 0.2.1-SNAPSHOT

--- full.cljs.async/project.clj
+++ full.cljs.async/project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.cljs.async "0.2.0"
+(defproject fullcontact/full.cljs.async "0.2.1-SNAPSHOT"
   :description "Extensions and helpers for cljs.core.async."
 
   :url "https://github.com/fullcontact/full.cljs"
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.cljs.parent "0.2.0"
+(defproject fullcontact/full.cljs.parent "0.2.1-SNAPSHOT"
   :description "ClojureScript sugar (full.monty's little brother)."
 
   :url "https://github.com/fullcontact/full.cljs"
