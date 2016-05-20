c3e8fb71ab5156790b36aad2fb4aed157b7d8901
Kaspars Dancis <kaspars@fullcontact.com>
2016-05-20 07:57:00 -0600

    http - support req with callback, passing additional params supported by ajax-request

--- full.cljs/src/full/cljs/http.cljs
+++ full.cljs/src/full/cljs/http.cljs
@@ -5,24 +5,30 @@
                                raw-response-format]]
             [full.cljs.json :refer [read-json write-json]]))
 
-(defn req>
-  [{:keys [url method params headers request-format response-format]
+(defn req
+  [{:keys [url method request-format response-format]
     :or {method :get
-         response-format (raw-response-format)}}]
+         response-format (raw-response-format)}
+    :as request}
+   callback]
+  (ajax-request (-> request
+                    (dissoc :url :request-format)
+                    (assoc :uri url
+                           :method method
+                           :format request-format
+                           :response-format response-format
+                           :handler (fn [[ok res]]
+                                      (-> (if ok
+                                            {:status 200
+                                             :body res}
+                                            {:status (:status res)
+                                             :body (:response res)})
+                                          (callback)))))))
+
+(defn req>
+  [request]
   (let [ch (chan 1)]
-    (ajax-request {:uri url
-                   :method method
-                   :params params
-                   :headers headers
-                   :format request-format
-                   :response-format response-format
-                   :handler (fn [[ok res]]
-                              (put! ch (if ok
-                                         {:status 200
-                                          :body res}
-                                         {:status (:status res)
-                                          :body (:response res)}))
-                              (close! ch))})
+    (req request (fn [response] (put! ch response) (close! ch)))
     ch))
 
 
@@ -37,12 +43,16 @@
 (defn json-response-format
   [& {:keys [json-key-fn] :or {json-key-fn ->kebab-case-keyword}}]
   {:read #(read-json (.getResponseText %) :json-key-fn json-key-fn)
-   ;:description "JSON"
    :content-type "application/json"})
 
+(defn- json-format [request]
+  (assoc request :request-format (json-request-format)
+                 :response-format (json-response-format)))
+
+(defn json-req
+  [request callback]
+  (req (json-format request) callback))
+
 (defn json-req>
-  [req]
-  (-> req
-      (assoc :request-format (json-request-format)
-             :response-format (json-response-format))
-      (req>)))
+  [request]
+  (req> (json-format request)))
